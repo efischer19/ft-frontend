b353caddef6d342a0b36a55c310793f435a3964f
Eric Fischer <efischer0119@gmail.com>
2018-07-07 16:32:03 -0400

    get unit tests working with my code

--- tests/unit/BlogTitle.spec.js
+++ tests/unit/BlogTitle.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import BlogTitle from '@/components/BlogTitle.vue';
 
-describe('HelloWorld.vue', () => {
+describe('BlogTitle.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'test message';
+    const wrapper = shallowMount(BlogTitle, {
       propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
