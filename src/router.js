fc405aa56434d2be5d8e129ee3470ba8ab52f39c
Eric Fischer <efischer0119@gmail.com>
2018-07-07 15:44:36 -0400

    clean up unused sample files and such

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import About from './views/About.vue';
 import Blog from './views/Blog.vue';
 
 Vue.use(Router);
@@ -13,16 +11,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-    {
-      path: '/blog',
       name: 'blog',
       component: Blog,
       props: {
