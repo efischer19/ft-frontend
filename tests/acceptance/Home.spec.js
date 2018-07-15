855a9e219bd64341a9e9167ce5ab9a6310bc5abf
Eric Fischer <efischer0119@gmail.com>
2018-07-15 18:49:48 -0400

    add snapshot test for home page

---
+++ tests/acceptance/Home.spec.js
@@ -0,0 +1,33 @@
+import { createRenderer } from 'vue-server-renderer';
+import Vue from 'vue';
+import mockAxios from 'jest-mock-axios';
+import Home from '@/views/Home.vue';
+import router from '@/router';
+
+const mockResponseObj = require('./posts.json');
+
+describe('Home', () => {
+  it('should match snapshot', () => {
+    const Constructor = Vue.extend(Home);
+    const vm = new Constructor({
+      router,
+      propsData: {
+        id: 'mc',
+      },
+    }).$mount();
+
+    expect(mockAxios.get).toHaveBeenCalledWith('/api/posts.json');
+    mockAxios.mockResponse({ data: mockResponseObj });
+
+    const renderer = createRenderer();
+    Vue.nextTick(() => {
+      renderer.renderToString(
+        vm,
+        (err, str) => {
+          if (err) throw new Error(err);
+          expect(str).toMatchSnapshot();
+        },
+      );
+    });
+  });
+});
